b7484992b4b2644013a4c0ec27b91270222a437b
Ethan Eagar <ethaneagar@yahoo.com>
2024-11-29 17:35:53 -0700

    Create ThreadScanParser.c
    Initial upload

---
+++ C/ThreadScanParser.c
@@ -0,0 +1,35 @@
+/*------------------------------------------------------------------------------
+	Function:	ThreadScanParser
+
+	This function will check and return the type of data that was scanned in.
+
+	Parameters:
+		scanData			- 	pointer to char, the scanned information
+
+	Return Value:
+		BARCODE_TYPE	-	see <Barcode Types>
+------------------------------------------------------------------------------*/
+int ThreadScanParser(char *scanData)
+{
+	int index;
+	
+	// check for each barcode type
+	for (index = 0; index < MAX_BARCODES; ++index)
+	{
+		// see if the scan is a match to a known barcode type
+		if (strcmp(scanData, BARCODE_STR[index]) == 0)
+		{
+			if (debugMode == TRUE)
+			{
+				// display status	
+				sprintf(statusStr, "scanner: FOUND known barcode (%s)", scanData);  	
+				StatusTable_Update(statusTable, statusStr, MESSAGE_PASS);	
+			}
+			return index;
+		}
+	} // end of for each barcode 
+	
+	// else the scan code be a serial, return to caller and let them figure it out
+	return BARCODE_SERIAL;
+	
+}
